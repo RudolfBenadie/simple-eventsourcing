3268e7a0b087cfabfcb6ccffbbb8cc27fa9dda74
Rudolf Benadie <rudolf.benadie@hotmail.com>
2022-11-04 10:58:09 +0000

    Remove distribution folder

--- event-publisher/dist/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-const server = new server_1.Server();
-server.start();
-//# sourceMappingURL=index.js.map
