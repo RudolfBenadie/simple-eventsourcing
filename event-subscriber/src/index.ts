62bc6be3ce339b8287d4e082348c934423ef5421
Rudolf Benadie <rudolf.benadie@hotmail.com>
2022-11-05 16:59:05 +0000

    Add repository to base code

--- event-subscriber/src/index.ts
+++ event-subscriber/src/index.ts
@@ -1,5 +1,8 @@
 import { Server } from './server';
+import { Repository } from './repository';
 const server = new Server();
+const repository = new Repository();
+
 server.start((messageTopic: Buffer, message: Buffer) => {
   const topic = messageTopic.toString();
   const payload = JSON.parse(message.toString());
